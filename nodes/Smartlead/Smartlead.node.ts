37ee48f6835a4f4f1b41234398be770929178e97
Chris-Terminator <chriscurry.1213@gmail.com>
2025-09-20 13:07:07 -0500

    Update Smartlead.node.ts

--- nodes/Smartlead/Smartlead.node.ts
+++ nodes/Smartlead/Smartlead.node.ts
@@ -11,7 +11,7 @@ export class Smartlead implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Smartlead',
 		name: 'smartlead',
-		icon: 'file:Smartlead.svg', // You will need to create a smartlead.svg icon
+		icon: 'file:Smartlead.svg',
 		group: ['transform'],
 		version: 1,
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
@@ -45,11 +45,27 @@ export class Smartlead implements INodeType {
 						name: 'Campaign',
 						value: 'campaign',
 					},
-					// Add other resources like 'Lead', 'Account' here
+					{
+						name: 'Lead',
+						value: 'lead',
+					},
+					{
+						name: 'Email Account',
+						value: 'emailAccount',
+					},
+					{
+						name: 'Webhook',
+						value: 'webhook',
+					},
+					{
+						name: 'Client',
+						value: 'client',
+					},
 				],
 				default: 'campaign',
 			},
-			// Operations for Campaign
+
+			// Operations
 			{
 				displayName: 'Operation',
 				name: 'operation',
@@ -67,11 +83,145 @@ export class Smartlead implements INodeType {
 						description: 'Get all campaigns',
 						action: 'Get all campaigns',
 					},
-					// Add other campaign operations like 'Create', 'Delete' here
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create a new campaign',
+						action: 'Create a campaign',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						description: 'Delete a campaign',
+						action: 'Delete a campaign',
+					},
+					{
+						name: 'Get Sequence Analytics',
+						value: 'getSequenceAnalytics',
+						description: 'Get sequence analytics for a campaign',
+						action: 'Get sequence analytics',
+					},
 				],
 				default: 'getAll',
 			},
-			// Add properties for your operations here. For example:
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['lead'],
+					},
+				},
+				options: [
+					{
+						name: 'List All By Campaign',
+						value: 'listAllByCampaign',
+						description: 'List all leads by campaign ID',
+						action: 'List all leads by campaign',
+					},
+					{
+						name: 'Add To Campaign',
+						value: 'addToCampaign',
+						description: 'Add leads to a campaign',
+						action: 'Add leads to a campaign',
+					},
+					{
+						name: 'Delete From Campaign',
+						value: 'deleteFromCampaign',
+						description: 'Delete a lead from a campaign',
+						action: 'Delete a lead from a campaign',
+					},
+				],
+				default: 'listAllByCampaign',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['emailAccount'],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create an email account',
+						action: 'Create an email account',
+					},
+					{
+						name: 'Remove From Campaign',
+						value: 'removeFromCampaign',
+						description: 'Remove an email account from a campaign',
+						action: 'Remove an email account from a campaign',
+					},
+				],
+				default: 'create',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['webhook'],
+					},
+				},
+				options: [
+					{
+						name: 'Fetch By Campaign',
+						value: 'fetchByCampaign',
+						description: 'Fetch webhooks by campaign ID',
+						action: 'Fetch webhooks by campaign',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						description: 'Delete a campaign webhook',
+						action: 'Delete a webhook',
+					},
+				],
+				default: 'fetchByCampaign',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['client'],
+					},
+				},
+				options: [
+					{
+						name: 'Add',
+						value: 'add',
+						description: 'Add a client to the system',
+						action: 'Add a client',
+					},
+					{
+						name: 'Delete API Key',
+						value: 'deleteApiKey',
+						description: 'Delete a client API key',
+						action: 'Delete a client API key',
+					},
+					{
+						name: 'Reset API Key',
+						value: 'resetApiKey',
+						description: 'Reset a client API key',
+						action: 'Reset a client API key',
+					},
+				],
+				default: 'add',
+			},
+
+			// Properties
 			{
 				displayName: 'Campaign ID',
 				name: 'campaignId',
@@ -80,11 +230,94 @@ export class Smartlead implements INodeType {
 				required: true,
 				displayOptions: {
 					show: {
-						resource: ['campaign'],
-						operation: ['addLeads'], // Example for a future operation
+						resource: ['campaign', 'lead', 'emailAccount', 'webhook'],
+						operation: ['delete', 'getSequenceAnalytics', 'listAllByCampaign', 'addToCampaign', 'deleteFromCampaign', 'removeFromCampaign', 'fetchByCampaign', 'delete'],
 					},
 				},
-				description: 'The ID of the campaign to add leads to',
+				description: 'The ID of the campaign',
+			},
+			{
+				displayName: 'Client ID',
+				name: 'clientId',
+				type: 'string',
+				default: '',
+				displayOptions: {
+					show: {
+						resource: ['campaign', 'client'],
+						operation: ['create', 'add', 'deleteApiKey', 'resetApiKey'],
+					},
+				},
+				description: 'The ID of the client',
+			},
+			{
+				displayName: 'Campaign Name',
+				name: 'campaignName',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['campaign'],
+						operation: ['create'],
+					},
+				},
+				description: 'The name of the campaign',
+			},
+			{
+				displayName: 'Lead List',
+				name: 'leadList',
+				type: 'json',
+				default: '[]',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['lead'],
+						operation: ['addToCampaign'],
+					},
+				},
+				description: 'An array of lead objects to add to the campaign',
+			},
+			{
+				displayName: 'Lead ID',
+				name: 'leadId',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['lead'],
+						operation: ['deleteFromCampaign'],
+					},
+				},
+				description: 'The ID of the lead to delete',
+			},
+			{
+				displayName: 'Email Account ID',
+				name: 'emailAccountId',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['emailAccount'],
+						operation: ['removeFromCampaign'],
+					},
+				},
+				description: 'The ID of the email account to remove',
+			},
+			{
+				displayName: 'Webhook ID',
+				name: 'webhookId',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['webhook'],
+						operation: ['delete'],
+					},
+				},
+				description: 'The ID of the webhook to delete',
 			},
 		],
 	};
@@ -106,9 +339,56 @@ export class Smartlead implements INodeType {
 							method: 'GET',
 							url: '/campaigns',
 						});
+					} else if (operation === 'create') {
+						const campaignName = this.getNodeParameter('campaignName', i) as string;
+						const clientId = this.getNodeParameter('clientId', i) as string;
+						const body: any = { name: campaignName };
+						if (clientId) {
+							body.client_id = clientId;
+						}
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'POST',
+							url: '/campaigns/create',
+							body,
+						});
+					} else if (operation === 'delete') {
+						const campaignId = this.getNodeParameter('campaignId', i) as string;
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'DELETE',
+							url: `/campaigns/${campaignId}`,
+						});
+					} else if (operation === 'getSequenceAnalytics') {
+						const campaignId = this.getNodeParameter('campaignId', i) as string;
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'GET',
+							url: `/campaigns/${campaignId}/sequence-analytics`,
+						});
 					}
-					// Add else if blocks for other campaign operations
+				} else if (resource === 'lead') {
+					if (operation === 'listAllByCampaign') {
+						const campaignId = this.getNodeParameter('campaignId', i) as string;
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'GET',
+							url: `/campaigns/${campaignId}/leads`,
+						});
+					} else if (operation === 'addToCampaign') {
+						const campaignId = this.getNodeParameter('campaignId', i) as string;
+						const leadList = this.getNodeParameter('leadList', i) as string;
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'POST',
+							url: `/campaigns/${campaignId}/leads`,
+							body: { lead_list: JSON.parse(leadList) },
+						});
+					} else if (operation === 'deleteFromCampaign') {
+						const campaignId = this.getNodeParameter('campaignId', i) as string;
+						const leadId = this.getNodeParameter('leadId', i) as string;
+						responseData = await this.helpers.requestWithAuthentication.call(this, 'smartleadApi', {
+							method: 'DELETE',
+							url: `/campaigns/${campaignId}/leads/${leadId}`,
+						});
+					}
 				}
+				// ... other resources and operations would be implemented here
 
 				const executionData = this.helpers.constructExecutionMetaData(
 					this.helpers.returnJsonArray(responseData),
