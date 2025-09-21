2ffd48deb62b5f8db79f4b03bddf70956befd428
Chris-Terminator <chriscurry.1213@gmail.com>
2025-09-21 18:03:57 +0000

    Potential fix for Operation fetch error

--- nodes/Smartlead/Smartlead.node.ts
+++ nodes/Smartlead/Smartlead.node.ts
@@ -56,15 +56,16 @@ export class Smartlead implements INodeType {
 				default: 'campaign',
 			},
 			{
-				displayName: 'Operation',
+				displayName: 'Operation Name or ID',
 				name: 'operation',
 				type: 'options',
+				description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
 				noDataExpression: true,
 				// The options are now loaded dynamically based on the resource selected
 				typeOptions: {
 					loadOptionsMethod: 'loadOperations',
 				},
-				default: 'getAll',
+				default: '',
 			},
 
 			// ====================================================================
@@ -654,7 +655,7 @@ export class Smartlead implements INodeType {
 	};
 
 	// Method to dynamically load operations based on the selected resource
-	async loadOperations(this: IExecuteFunctions): Promise<INodePropertyOptions[]> {
+	async loadOperations(this: INodePropertyOptions): Promise<INodePropertyOptions[]> {
 		const resource = this.getNodeParameter('resource', 0, '') as string;
 		if (resource === 'campaign') {
 			return [
